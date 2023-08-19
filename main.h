655357186089ff90ebe7dc5c6d323a764ebb95c9
Merna-Fadl <merna.fadl1996@gmail.com>
2023-08-19 18:38:54 +0300

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -4,19 +4,6 @@
 #include <stdlib.h>
 #include <starg.h>
 
-/**
- * struct specifier - struct defines specifier for format
- * @specifier: format
- * @f: function associated
- */
-
-typedef struct specifier
-{
-	char *specifier;
-
-	void(f)(char *, va_list);
-
-} specifier_t;
 
 int _printf(const char *format, ...);
 
