b78e2a4ba8408997788bb39edba9e7e1f7c22121
Mahmoud <9799.mah@gmail.com>
2023-08-19 13:04:55 +0300

    Updated

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ */
+int _printf(const char *format, ...);
+
+#endif /* _MAIN_H_ */
