de4df364257db2ecb458e11e82c578c2587a527e
Mahmoud <9799.mah@gmail.com>
2023-08-21 21:13:17 +0300

    Done

--- _printf.c
+++ _printf.c
@@ -1,53 +1,69 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed
+ * (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list args;
+	int count = 0;
 
-	if (format == NULL)
-		return (-1);
+	va_start(args, format);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			format++;
+
+			if (*format == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			else if (*format == 'c')
+			{
+				count += putchar(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				while (*str)
+				{
+					count += putchar(*str);
+					str++;
+				}
+			}
+			else if (*format == '%')
+			{
+				count += putchar('%');
+			}
+			else
+			{
+				count += putchar('%');
+				count += putchar(*format);
+			}
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			count += putchar(*format);
 		}
+
+		format++;
 	}
 
-	print_buffer(buffer, &buff_ind);
+	va_end(args);
 
-	va_end(list);
-
-	return (printed_chars);
+	return (count);
 }
