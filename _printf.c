b3558fee1b0988d9c57185a445aa2fb7359b1820
Merna-Fadl <merna.fadl1996@gmail.com>
2023-08-22 09:41:00 +0300

    printf project tesk zero

--- _printf.c
+++ _printf.c
@@ -1,46 +1,56 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
  * _printf - Produces output according to a format.
  * @format: The format string.
+ *
  * Return: The number of characters printed
  * (excluding the null byte used to end output to strings).
+ *
  */
 int _printf(const char *format, ...)
-{va_list args;
+{
+	va_list args;
 	int count = 0;
 
-	if (format == NULL)
-		return (-1);
 	va_start(args, format);
-	while (*format)
+
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0')
-				return (-1);
-			switch (*format)
+			if (*format == 'c')
 			{
-				case 'c':
-					count += putchar(va_arg(args, int));
-					break;
-				case 's': {
-					char *str = va_arg(args, char *);
+				count += putchar(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				count = va_arg(args, char *);
 
-					if (str == NULL)
-						str = "(NULL)";
-					count += printf("%s", str);
-					break; }
-				case '%':
-					count += putchar('%');
-					break;
-				default:
-					count += putchar('%');
-					count += putchar(*format);
-					break; }}
+
+			}
+			else if (*format == '%')
+			{
+				count += putchar('%');
+			}
+			else
+			{
+				count += putchar('%');
+				count += putchar(*format);
+			}
+		}
 		else
 		{
-			count += putchar(*format); }
-		format++; }
+			count += putchar(*format);
+		}
+
+		format++;
+	}
+
 	va_end(args);
-	return (count); }
+	return (count);
+}
+
